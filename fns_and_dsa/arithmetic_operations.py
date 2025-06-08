4195aa2887dbc66f9ad40d0ef33684025de951c4
anas amellahe <amellaheanas@gmail.com>
2025-06-08 19:07:06 +0100

    fix division by zero

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,8 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return "Error: Can't divide by 0."
-            return num1 / num2
+            elif num2 != 0:
+                return num1 / num2
         case "subtract":
             return num1 - num2
         case _:

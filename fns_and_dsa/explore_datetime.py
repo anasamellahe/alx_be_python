b31f2bd4d2ce0287fd7ca65dcca3cdb5e6c295cd
anas amellahe <amellaheanas@gmail.com>
2025-06-09 00:22:42 +0100

    add the from keyword

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,12 +1,12 @@
-import  datetime
+from datetime import  datetime, timedelta
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     formatted = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print("Current date and time:",formatted)
 
 def calculate_future_date(daysToAdd):
-    future_date = datetime.datetime.now() + datetime.timedelta(days=daysToAdd)
+    future_date = datetime.now() + timedelta(days=daysToAdd)
     print("Future date:", future_date.strftime("%Y-%m-%d"))
 
 

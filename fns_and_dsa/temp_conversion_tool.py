dc32d623f8f7217a7032a4e2367f3fbcee9302ec
anas amellahe <amellaheanas@gmail.com>
2025-06-09 00:29:45 +0100

    fix Global conversion factors

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
 # Global conversion factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 # Function to convert Fahrenheit to Celsius
 def convert_to_celsius(fahrenheit):
@@ -33,3 +33,4 @@ if temperature_input.replace('.', '', 1).isdigit():
 
 else:
     print("Invalid temperature. Please enter a numeric value.")
+
